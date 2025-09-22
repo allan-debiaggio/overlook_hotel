46e35b792360689b1aef749b0f7b951500193baf
mathieu auger <mathieu.auger@laplateforme.io>
2025-09-22 10:10:18 +0200

    road for connection

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,5 +1,20 @@
 package com.example.demo.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
 public class SecurityConfig {
-    
+    @Bean
+SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(csrf -> csrf.disable()) // indispensable si tu testes avec Postman
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/register").permitAll() // autorisé sans login
+            .anyRequest().authenticated()
+        )
+        .formLogin(Customizer.withDefaults()); // optionnel si tu veux garder /login HTML
+    return http.build();
+}
 }
--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,6 +1,21 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
 //ici ajouter une route permettant de recevoir dans postman "Coucou l'ami"
+@RestController
 public class AuthController {
+    @PostMapping("/register")
+    public RegisterRequest register(@RequestBody RegisterRequest req) {
     
+        System.out.println(req);
+        return req;
+    
+
+    
+}
+public record RegisterRequest(String username, String password) {}
 }
