5848743cc678ae18a6f7ac66e23a0d5d89f0ac5d
daria-marchenko-d <daria.marchenko@laplateforme.io>
2025-09-23 16:53:02 +0200

    Add login and register page

---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class AuthController {
+
+    
+    @PostMapping("/register")
+    public AuthRequest register(@RequestBody AuthRequest req) {
+    
+        System.out.println(req);
+        return req;
+    }
+    @PostMapping("/admin/login")
+    public AuthRequest admLoginRequest(@RequestBody AuthRequest req){
+        return req;
+        
+    }
+
+    @PostMapping("/user/login")
+    public AuthRequest UserLoginRequest(@RequestBody AuthRequest req){
+        return req;
+        
+    }
+
+    public record AuthRequest(String username, String password) {}
+    
+}
