498ecfd70193d94a44e951d7ce8435dc244bb052
mathieu auger <mathieu.auger@laplateforme.io>
2025-09-29 09:12:50 +0200

    correction

--- src/main/java/com/example/demo/controller/KpiController.java
+++ src/main/java/com/example/demo/controller/KpiController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/kpis")
+@RequestMapping("/api/kpi")
 public class KpiController {
 
     private final KpiService service;
